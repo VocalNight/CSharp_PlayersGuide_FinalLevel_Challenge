b93084a701ebaf006407871076418661129a9d82
Vocal <vocalnight@protonmail.com>
2023-05-26 23:42:48 -0300

    Actions system first implementantion

--- Game.cs
+++ Game.cs
@@ -24,20 +24,35 @@ namespace CSharp_PlayersGuide_FinalLevel_Challenge
             {
                 foreach (var npc in this._heroes)
                 {
-                    Console.WriteLine($"It's {npc.Name}1 turn..");
-                    Console.WriteLine($"{npc.Name} did nothing");
+                    Console.WriteLine($"It's {npc.Name} turn..");
+                    Action("Player 1", npc);
                 }
             } else
             {
                 foreach(var npc in this._monsters)
                 {
                     Console.WriteLine($"It's {npc.Name} turn..");
-                    Console.WriteLine($"{npc.Name} did nothing");
+                    Action("Player 2", npc);
                 }
             }
 
             Turn = !Turn;
             Thread.Sleep( 1000 );
+        }
+
+        private void Action(string player, Npc npc)
+        {
+            Console.WriteLine("What would you like to do? ");
+
+            // ConsoleKey key = Console.ReadKey().Key;
+            ConsoleKey key = Random.Shared.Next(0, 1) == -1 ? ConsoleKey.NumPad1 : ConsoleKey.NumPad1;
+
+            switch (key)
+            {
+                case ConsoleKey.NumPad1:
+                    Console.WriteLine($"{npc.Name} did nothing");
+                    break;
+            }
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using CSharp_PlayersGuide_FinalLevel_Challenge;
 
+Console.Write("What's the name of the player character? ");
+string? playerName = Console.ReadLine();
+
 Monster skeleton = new Monster("Skeleton", 1);
-Hero hero = new Hero("Skeleton", 1);
+Hero hero = new Hero(playerName, 1);
+
 
 List<Hero> heroes = new List<Hero> { hero };
 List<Monster> monsters = new List<Monster> { skeleton };
